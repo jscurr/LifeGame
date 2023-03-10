3e8f614b8c02c19fb8bdce209862d414824f54d3
jscurr <joelscurr@gmail.com>
2023-03-09 20:07:43 -0900

    First commit of this project

---
+++ src/com/game_package/App.java
@@ -0,0 +1,13 @@
+package com.game_package;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        
+    }
+}
---
+++ src/com/game_package/LifePanel.java
@@ -0,0 +1,10 @@
+package com.game_package;
+
+import javax.swing.JPanel;
+
+public class LifePanel extends JPanel{
+
+    //TODO Finish this class and then upload the project to Github
+    
+    
+}
